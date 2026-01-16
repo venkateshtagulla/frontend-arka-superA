9b8485a18bc2c7a1dc36b49877cf3da3d58c12cc
venkateshtagulla <154754274+venkateshtagulla@users.noreply.github.com>
2026-01-16 21:09:36 +0530

    Update util.js

--- src/util.js
+++ src/util.js
@@ -21,7 +21,7 @@ export const getBearerToken = async () => {
 
   try {
     const res = await axios.post(
-https://tirplubeoa.execute-api.ap-south-2.amazonaws.com+"/user/refresh",
+  "https://tirplubeoa.execute-api.ap-south-2.amazonaws.com/user/refresh",
       {
         refresh_token: refreshToken,
       }
