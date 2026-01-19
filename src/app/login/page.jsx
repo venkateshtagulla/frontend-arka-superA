1bc3b3aa5b5a429b4871905622758294a87e2481
venkateshtagulla <154754274+venkateshtagulla@users.noreply.github.com>
2026-01-19 12:03:20 +0530

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -19,7 +19,7 @@ export default function Login() {
     }
     try {
       const response = await axios.post(
-        process.env.NEXT_PUBLIC_BACKEND_URL + "/user/login",
+        process.env.NEXT_PUBLIC_BACKEND_URL + "/login",
         payload
       );
       const { access_token, id_token, refresh_token } = response.data;
